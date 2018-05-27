4477248a8e13a1960f2d398971001be6c8375941
Iulian Ilea <iulian@f9software.com>
2018-05-27 12:03:16 +0300

    Added setValues and getField on Form.

--- src/Form.ts
+++ src/Form.ts
@@ -12,4 +12,30 @@ export class Form {
         // if any of the fields is invalid, then the entire form is invalid
         return !this.fields.getRange().some(field => !field.isValid());
     }
+
+    /**
+     * Sets fields values in bulk.
+     * @param {{[p: string]: any}} data
+     */
+    setValues(data: {[key: string]: any}): void {
+        Object.keys(data)
+            .forEach(fieldName => {
+                const field = this.getField(fieldName);
+
+                if (!field) {
+                    throw 'Field "' + fieldName + '" does not exist on this form.';
+                }
+
+                field.setValue(data[fieldName]);
+            });
+    }
+
+    /**
+     *
+     * @param {string} fieldName
+     * @returns {Field | undefined}
+     */
+    getField(fieldName: string): Field | undefined {
+        return this.fields.get(fieldName);
+    }
 }
